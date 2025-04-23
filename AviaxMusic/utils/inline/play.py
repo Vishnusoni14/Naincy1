43034fa27ec5a5d1f4f5ddacd724a77cf09b98b8
NAINCY <vishnu14sahil25love28@gmail.com>
2025-04-23 12:23:55 +0530

    Update play.py

--- AviaxMusic/utils/inline/play.py
+++ AviaxMusic/utils/inline/play.py
@@ -81,7 +81,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
+            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/MUNNA_MUSIC_BOT?startgroup=true",)
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
         ],
     ]
